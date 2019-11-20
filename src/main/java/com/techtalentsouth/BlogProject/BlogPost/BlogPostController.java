457ee9de0206d64c491b1c1e814d98db5122a601
Ryan Strobolakos <ryanstrobolakos@Ryans-Air.lan>
2019-11-20 13:51:37 -0500

    Adding GetMapping annotation to BlogPostController class

--- src/main/java/com/techtalentsouth/BlogProject/BlogPost/BlogPostController.java
+++ src/main/java/com/techtalentsouth/BlogProject/BlogPost/BlogPostController.java
@@ -1,10 +1,12 @@
 package com.techtalentsouth.BlogProject.BlogPost;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BlogPostController {
 
+	@GetMapping(value="/")
 	public String index(BlogPost blogPost) {
 		return "blogpost/index";
 	}
