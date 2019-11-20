50ad83836358ef717373d52209b4aef78bb5b3b6
Ryan Strobolakos <ryanstrobolakos@Ryans-Air.lan>
2019-11-20 13:19:49 -0500

    Initial commit

---
+++ src/test/java/com/techtalentsouth/BlogProject/BlogProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtalentsouth.BlogProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
